92865842d440a6536e2f26bb7751fc4f112c0c4e
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2024-04-19 17:58:27 +0200

    ACTUALIZACION DE BASE DE DATOS

--- BibliotecaIETI/BibliotecaApp/admin.py
+++ BibliotecaIETI/BibliotecaApp/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import User, TipusElement, ElementCatalog, Exemplar, Reserva, Prestec, Peticio, Registre
+from .models import User, ItemCatalogo, Libro, CD, DVD, BR, Dispositivo, Ejemplar, Reserva, Prestamo, Peticion, Log
+
+
+
 
 admin.site.register(User)
-admin.site.register(TipusElement)
-admin.site.register(ElementCatalog)
-admin.site.register(Exemplar)
+admin.site.register(ItemCatalogo)
+admin.site.register(Libro)
+admin.site.register(CD)
+admin.site.register(DVD)
+admin.site.register(BR)
+admin.site.register(Dispositivo)
+admin.site.register(Ejemplar)
 admin.site.register(Reserva)
-admin.site.register(Prestec)
-admin.site.register(Peticio)
-admin.site.register(Registre)
+admin.site.register(Prestamo)
+admin.site.register(Peticion)
+admin.site.register(Log)
+
--- BibliotecaIETI/BibliotecaApp/models.py
+++ BibliotecaIETI/BibliotecaApp/models.py
@@ -1,43 +1,87 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Modelo base para los elementos del catálogo
+class ItemCatalogo(models.Model):
+    id_catalogo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200)
+    ocio = models.TextField()
+    autor = models.CharField(max_length=200)
+    data_edicion = models.DateField()
+
+# Modelo para los libros
+class Libro(ItemCatalogo):
+    CDU = models.CharField(max_length=100)
+    ISBN = models.CharField(max_length=13)
+    editorial = models.CharField(max_length=100)
+    coleccion = models.CharField(max_length=100)
+    paginas = models.IntegerField()
+
+# Modelo para los CD
+class CD(ItemCatalogo):
+    discografica = models.CharField(max_length=100)
+    estilo = models.CharField(max_length=100)
+    duracion = models.TimeField()
+
+# Modelo para los DVD
+class DVD(ItemCatalogo):
+    director = models.CharField(max_length=100)
+    duracion = models.DurationField()
+    subtitulos = models.BooleanField(default=True)
+    idiomas_audio = models.CharField(max_length=100)
+    formato_video = models.CharField(max_length=50)
+
+# Modelo para los Blu-ray
+class BR(ItemCatalogo):
+    estudio = models.CharField(max_length=100)
+    formato_video = models.CharField(max_length=50)
+    extras = models.TextField()
+
+# Modelo para los dispositivos
+class Dispositivo(ItemCatalogo):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    tipo_conexion = models.CharField(max_length=100)
+    sistema_operativo = models.CharField(max_length=100)
+    almacenamiento = models.CharField(max_length=100)
+
+# Modelo para los ejemplares
+class Ejemplar(models.Model):
+    elemento = models.ForeignKey(ItemCatalogo, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=200)
+    disponible = models.BooleanField(default=True)
 
 class User(AbstractUser):
-    birthdate = models.DateField()
-    center = models.CharField(max_length=100)
-    cycle = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField(null=True)
+    centro = models.CharField(max_length=100)
+    ciclo = models.CharField(max_length=100)
+    roles = models.CharField(max_length=100)
     image = models.ImageField(upload_to='profile_photos', default='default.jpg')
 
     # Definir accesos inversos personalizados para evitar conflictos
     groups = models.ManyToManyField('auth.Group', related_name="biblioteca_user_groups", blank=True)
     user_permissions = models.ManyToManyField('auth.Permission', related_name="biblioteca_user_permissions", blank=True)
-    
-class TipusElement(models.Model):
-    nom = models.CharField(max_length=100)
-
-class ElementCatalog(models.Model):
-    tipus_element = models.ForeignKey(TipusElement, on_delete=models.CASCADE)
-    nom = models.CharField(max_length=100)
-    imatge = models.ImageField(upload_to='catalog_images', default='default_item.jpg')
-
-class Exemplar(models.Model):
-    element_catalog = models.ForeignKey(ElementCatalog, on_delete=models.CASCADE)
 
 class Reserva(models.Model):
-    usuari = models.ForeignKey(User, on_delete=models.CASCADE)
-    exemplar = models.ForeignKey(Exemplar, on_delete=models.CASCADE)
-    data_reserva = models.DateField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    ejemplar = models.ForeignKey(Ejemplar, on_delete=models.CASCADE)
+    fecha_reserva = models.DateTimeField(auto_now_add=True)
 
-class Prestec(models.Model):
-    usuari = models.ForeignKey(User, on_delete=models.CASCADE)
-    exemplar = models.ForeignKey(Exemplar, on_delete=models.CASCADE)
-    data_prestec = models.DateField()
-    data_retorn = models.DateField(null=True, blank=True)
+class Prestamo(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    ejemplar = models.ForeignKey(Ejemplar, on_delete=models.CASCADE)
+    fecha_prestamo = models.DateTimeField(auto_now_add=True)
+    fecha_devolucion = models.DateTimeField(null=True, blank=True)
 
-class Peticio(models.Model):
-    usuari = models.ForeignKey(User, on_delete=models.CASCADE)
-    data_peticio = models.DateField()
-    complerta = models.BooleanField(default=False)
+class Peticion(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    elemento = models.ForeignKey(ItemCatalogo, on_delete=models.CASCADE)
+    fecha_peticion = models.DateTimeField(auto_now_add=True)
 
-class Registre(models.Model):
-    missatge = models.TextField()
-    data_registre = models.DateTimeField(auto_now_add=True)
+from django.db import models
+
+class Log(models.Model):
+    evento = models.CharField(max_length=200)
+    nivel = models.CharField(max_length=20)  
+    fecha_registro = models.DateTimeField(auto_now_add=True)
